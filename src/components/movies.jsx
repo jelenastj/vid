ce366c9aad0d63902860664f8a3b6d4f95473cff
jelenastj <jelena.stjepanovic@gmail.com>
2021-01-13 11:18:05 -0800

    pagination 3

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
 import Like from './common/like';
-import Pagination from './common/pagination'
+import Pagination from './common/pagination';
 
 class Movies extends Component {
 	state = {
-    movies: getMovies(),
-    pageSize: 4,
+		movies: getMovies(),
+		pageSize: 4,
+		currentPage: 1,
 	};
 	handleDelete = (movie) => {
 		const movies = this.state.movies.filter((m) => m._id !== movie._id);
 		this.setState({ movies });
 	};
-  handleLike =(movie)=>{
-    const movies = [...this.state.movies];
-    const index = movies.indexOf(movie);
-    movies[index] = {...movies[index]};
-    movies[index].liked = !movies[index].liked; 
-    this.setState( {movies })
-  }
-  handlePageChange =(page) => {
-    console.log(page);
-  }
+	handleLike = (movie) => {
+		const movies = [...this.state.movies];
+		const index = movies.indexOf(movie);
+		movies[index] = { ...movies[index] };
+		movies[index].liked = !movies[index].liked;
+		this.setState({ movies });
+	};
+	handlePageChange = (page) => {
+		this.setState({ currentPage: page });
+	};
 	render() {
 		const { length: count } = this.state.movies;
+		const { currentPage, pageSize } = this.state;
 
 		if (count === 0) return <p>There are no movies in the database.</p>;
 		return (
@@ -48,9 +50,7 @@ class Movies extends Component {
 								<td>{movie.numberInStock}</td>
 								<td>{movie.dailyRentalRate}</td>
 								<td>
-                  <Like 
-                  liked={movie.liked} 
-                  onClick={()=>this.handleLike(movie)} />
+									<Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
 								</td>
 								<td>
 									<button onClick={() => this.handleDelete(movie)} className='btn btn-danger btn-sm'>
@@ -61,7 +61,12 @@ class Movies extends Component {
 						))}
 					</tbody>
 				</table>
-        <Pagination itemsCount={count} pageSize={this.state.pageSize} onPageChange={this.handlePageChange}/>
+				<Pagination
+					itemsCount={count}
+					pageSize={pageSize}
+					currentPage={currentPage}
+					onPageChange={this.handlePageChange}
+				/>
 			</div>
 		);
 	}
