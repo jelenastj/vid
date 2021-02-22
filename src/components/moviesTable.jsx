1dac9da0cb9108046763816399030d3cccc1ec85
jelenastj <jelena.stjepanovic@gmail.com>
2021-02-21 19:13:13 -0800

    extracting table

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,36 +1,33 @@
-import React, { Component   } from 'react';
-import TableHeader from "./common/tableHeader"
+import React, { Component } from 'react';
 import Like from './common/like';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 
- class MoviesTable extends Component {
-   columns = [
-     { path: 'title', label: 'Title' },
-     { path: 'genre.name', label: 'Genre' },
-     { path: 'numberInStock', label: 'Stock' },
-     { path: 'dailyRentalRate', label: 'Rate' },
-     { key: 'like', 
-     content: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />},
-     { key: 'delete', 
-     content: movie => <button onClick={() => this.props.onDelete(movie)} className='btn btn-danger btn-sm'>
-     Delete
-   </button>}
-  ]
-  
-  render() {
-    const { movies, sortColumn, onSort } = this.props;
-    return (
-      <div>
-        <table className='table'>
-      <TableHeader 
+class MoviesTable extends Component {
+	columns = [
+		{ path: 'title', label: 'Title' },
+		{ path: 'genre.name', label: 'Genre' },
+		{ path: 'numberInStock', label: 'Stock' },
+		{ path: 'dailyRentalRate', label: 'Rate' },
+		{ key: 'like', content: (movie) => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
+		{
+			key: 'delete',
+			content: (movie) => (
+				<button onClick={() => this.props.onDelete(movie)} className='btn btn-danger btn-sm'>
+					Delete
+				</button>
+			),
+		},
+	];
+
+	render() {
+		const { movies, sortColumn, onSort } = this.props;
+		return (
+      <Table 
       columns={this.columns} 
+      data={movies} 
       sortColumn={sortColumn} 
-      onSort = {onSort} />
-      <TableBody data ={movies} columns={this.columns}/>
-		</table>
-
-      </div>
-    )
-  }
+      onSort={onSort}/>
+		);
+	}
 }
-export default MoviesTable
+export default MoviesTable;
