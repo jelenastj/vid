b1487e5863dcbaed182a2aaf84f52bbf30dc18aa
jelenastj <jelena.stjepanovic@gmail.com>
2021-02-25 18:53:41 -0800

    form

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -14,6 +14,7 @@ import { Link, NavLink} from 'react-router-dom'
       <NavLink className="nav-item nav-link " to="/movies">Movies <span className="sr-only">(current)</span></NavLink>
       <NavLink className="nav-item nav-link" to="/customers">Customers</NavLink>
       <NavLink className="nav-item nav-link" to="/rentals">Rentals</NavLink>
+      <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
 
     </div>
   </div>
