7fd9a48daeeffc3fb59b3b940cc26fcb3e256974
jelenastj <jelena.stjepanovic@gmail.com>
2021-02-25 19:44:53 -0800

    controlled elements

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,28 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
- class LoginForm extends Component {
-   username= React.createRef();
-
-   componentDidMount(){
-     this.username.current.focus()
-   }
-   handleSubmit = e => {
-     e.preventDefault()
-     const username = this.username.current.value;
-   };
-  render() {
-    return (
-     <div>
-       <h1>login</h1>
-       <form onSubmit={this.handleSubmit}>
-         <div className="form-group"><label htmlFor="username">Username</label>
-         <input ref={this.username}id="username" type="text" className="form-control"/></div>
-         <div className="form-group"><label htmlFor="password">Password</label>
-         <input id="password" type="text" className="form-control"/></div>
-       </form>
-       <button className="btn btn-primary">Login</button>
-     </div>
-    )
+class LoginForm extends Component {
+	state = {
+		account: { username: '', password: '' },
+	};
+	handleSubmit = (e) => {
+		e.preventDefault();
+	};
+  handleChange = e => {
+   const account ={...this.state.account};
+   account.username = e.target.value;
+   this.setState({account})
   }
+	render() {
+		return (
+			<div>
+				<h1>login</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className='form-group'>
+						<label htmlFor='username'>Username</label>
+						<input 
+            value={this.state.account.username} 
+            onChange={this.handleChange}
+              id='username' 
+              type='text' 
+              className='form-control' />
+					</div>
+					<div className='form-group'>
+						<label htmlFor='password'>Password</label>
+						<input 
+              id='password' 
+              type='text' 
+              className='form-control' />
+					</div>
+				</form>
+				<button className='btn btn-primary'>Login</button>
+			</div>
+		);
+	}
 }
-export default LoginForm
+export default LoginForm;
