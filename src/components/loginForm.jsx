e3f5f5439bc72c43a0dcdf9b609f583941daf7f1
jelenastj <jelena.stjepanovic@gmail.com>
2021-02-26 19:55:26 -0800

    validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -29,10 +29,25 @@ class LoginForm extends Component {
     
 	};
 
+  validateProperty = ({name, value}) => {
+    if(name === 'username'){
+      if(value.trim()==='')  return "Username is required."
+    } 
+    if(name === 'password'){
+      if(value.trim()==='')  return "Password is required."
+  }
+}
+
 	handleChange = ({ target: input }) => {
+    const errors = { ...this.state.errors }
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
 		const account = { ...this.state.account };
 		account[input.name] = input.value;
-		this.setState({ account });
+
+		this.setState({ account, errors });
 	};
 
 
