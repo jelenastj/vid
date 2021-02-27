8431fd39e6f070d6a016fec26fb449657d2ff390
jelenastj <jelena.stjepanovic@gmail.com>
2021-02-26 19:41:13 -0800

    validation implementation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,36 +1,60 @@
 import React, { Component } from 'react';
-import Input from './common/input'
+import Input from './common/input';
 
 class LoginForm extends Component {
 	state = {
-		account: { username: '', password: '' },
-	};
+		account: { username: "", password: "" },
+    errors: {}
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const {account} = this.state;
+    if(account.username.trim() ==='')
+    errors.username = 'Username is required'
+    if(account.password.trim()==='')
+    errors.password = 'Password is required'
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+  
+
 	handleSubmit = (e) => {
 		e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors });
+    if (errors) return;
+    
 	};
-  handleChange = ({target: input}) => {
-   const account ={...this.state.account};
-   account[input.name] = input.value;
-   this.setState({account})
-  }
+
+	handleChange = ({ target: input }) => {
+		const account = { ...this.state.account };
+		account[input.name] = input.value;
+		this.setState({ account });
+	};
+
+
 	render() {
-    const {account} = this.state
+		const { account } = this.state;
 		return (
 			<div>
 				<h1>login</h1>
 				<form onSubmit={this.handleSubmit}>
-				<Input 
-          name='username' 
-          value={account.username} 
-          label='Username' 
-          onChange={this.handleChange}/>
-          <Input 
+					<Input 
+            name='username' 
+            value={account.username} 
+            label='Username' 
+            onChange={this.handleChange} />
+					<Input 
             name='password' 
             value={account.password} 
             label='Password' 
-            onChange={this.handleChange}/>	
-				</form>
+            onChange={this.handleChange} />
 				<button className='btn btn-primary'>Login</button>
+				</form>
 			</div>
 		);
 	}
